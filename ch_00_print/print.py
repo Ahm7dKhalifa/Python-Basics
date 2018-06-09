544ca2eedda15cc9fd310ff2615dfc2eb34382fb
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-06-09 12:19:07 +0200

    ADD ch_00_print

---
+++ ch_00_print/print.py
@@ -0,0 +1,102 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#1.print with numbers
+
+
+# In[2]:
+
+#1.1 print Fixed value
+
+
+# In[3]:
+
+print(123)
+
+
+# In[4]:
+
+#1.2 print result of arithmetic operations
+
+
+# In[5]:
+
+print(10 + 10)
+
+
+# In[6]:
+
+print( 25 / 5)
+
+
+# In[7]:
+
+#2.print with String
+
+
+# In[8]:
+
+#2.1 Print With double quotes
+
+
+# In[9]:
+
+print(" Hello World ")
+
+
+# In[10]:
+
+print(" 10 + 10 ")
+
+
+# In[11]:
+
+#2.2 Print With single quotes
+
+
+# In[12]:
+
+print('ahmed khalifa')
+
+
+# In[13]:
+
+#2.3 print single quotes inside double quotes
+
+
+# In[14]:
+
+print(" my name is 'ahmed khalifa' ")
+
+
+# In[15]:
+
+#2.4 print double quotes inside single quotes
+
+
+# In[16]:
+
+print('my name is "ahmed khalifa"')
+
+
+# In[17]:
+
+#2.5 using Escape Characters
+
+
+# In[18]:
+
+print(" my name is \"ahmed khalifa\" ")
+
+
+# In[20]:
+
+print("write my name in new line : \n ahmed khalifa")
+
+
+# In[ ]:
+
+
+
