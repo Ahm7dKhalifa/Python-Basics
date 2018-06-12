e9bbe1bb7c478a725f6089a437d8ec753f85ce28
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2018-06-12 13:21:08 +0200

    ADD ch_02_Variables and DataTypes

---
+++ ch_02_Variables and DataTypes/Project/Variables+and+DataTyps.py
@@ -0,0 +1,121 @@
+
+# coding: utf-8
+
+# # 1.variables with numbers
+
+# In[2]:
+
+positiveInteger = 10
+
+
+# In[3]:
+
+negativeInteger = -10
+
+
+# In[4]:
+
+positiveFloat = 123.567
+
+
+# In[5]:
+
+negativeFloat = -123.567
+
+
+# In[6]:
+
+complexNumber = 5 + 3j
+
+
+# In[7]:
+
+type(positiveInteger )
+
+
+# In[8]:
+
+type(negativeInteger)
+
+
+# In[9]:
+
+type(positiveFloat)
+
+
+# In[10]:
+
+type(negativeFloat)
+
+
+# In[11]:
+
+type(complexNumber)
+
+
+# # 2.variables With String
+
+# In[13]:
+
+myName = "ahmed khalifa"
+
+
+# In[14]:
+
+myName2 = 'ahmed khalifa'
+
+
+# In[15]:
+
+myName3 = "ahmed 'khalifa'"
+
+
+# In[16]:
+
+myName4 = 'ahmed "khalifa"'
+
+
+# In[17]:
+
+myName5 = "ahmed \"khalifa\""
+
+
+# In[18]:
+
+type(myName)
+
+
+# In[19]:
+
+print(myName5)
+
+
+# In[20]:
+
+print(myName[0])
+
+
+# In[22]:
+
+print(myName[6])
+
+
+# In[23]:
+
+print(myName[0:6])
+
+
+# In[24]:
+
+print(myName[6:])
+
+
+# In[25]:
+
+print(myName[:6])
+
+
+# In[ ]:
+
+
+
